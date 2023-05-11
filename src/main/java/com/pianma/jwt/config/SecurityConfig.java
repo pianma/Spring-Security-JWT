49b6cf3084afe39b8caec05ac422c52c6eb661e2
KimGeonWoo <ikkw2040@naver.com>
2023-05-11 22:24:09 +0900

    CorsConfig 생성

--- src/main/java/com/pianma/jwt/config/SecurityConfig.java
+++ src/main/java/com/pianma/jwt/config/SecurityConfig.java
@@ -1,21 +1,27 @@
 package com.pianma.jwt.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.filter.CorsFilter;
 
 @Configuration // IoC 빈(bean)을 등록
 @EnableWebSecurity // 스프링 시큐리티 필터가 스프링 필터체인에 등록이 됨
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final CorsFilter corsFilter;
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
         http.csrf().disable();
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
+                .addFilter(corsFilter)
                 .formLogin().disable()
                 .httpBasic().disable()
                 .authorizeRequests()
