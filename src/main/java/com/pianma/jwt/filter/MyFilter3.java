c0be8e0ff64af343ca352132d5b0b6f41475783b
KimGeonWoo <ikkw2040@naver.com>
2023-05-29 21:14:06 +0900

    jwt 임시 토큰 테스트

--- src/main/java/com/pianma/jwt/filter/MyFilter3.java
+++ src/main/java/com/pianma/jwt/filter/MyFilter3.java
@@ -1,12 +1,29 @@
 package com.pianma.jwt.filter;
 
 import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 public class MyFilter3 implements Filter{
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
-        System.out.println("필터 3");
-        filterChain.doFilter(servletRequest, servletResponse);
+
+        HttpServletRequest req = (HttpServletRequest)servletRequest;
+        HttpServletResponse res = (HttpServletResponse)servletResponse;
+
+        if(req.getMethod().equals("POST")){
+            System.out.println("post 요청됨");
+            String headerAuth =  req.getHeader("Authorization");
+            System.out.println(headerAuth);
+
+            if(headerAuth.equals("cos")){
+                filterChain.doFilter(req, res);
+            }else{
+                PrintWriter out = res.getWriter();
+                out.println("인증안됨");
+            }
+        }
     }
 }
