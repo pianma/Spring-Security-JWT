4ad2589de19ad5d746f4331e69c0cb3879b7f8c5
KimGeonWoo <ikkw2040@naver.com>
2023-06-12 23:25:41 +0900

    토큰 생성, 로그인 시도

--- src/main/java/com/pianma/jwt/JwtAuthenticationFilter.java
+++ src/main/java/com/pianma/jwt/JwtAuthenticationFilter.java
@@ -1,9 +1,11 @@
 package com.pianma.jwt;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.pianma.jwt.auth.PrincipalDetails;
 import com.pianma.jwt.model.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -43,13 +45,27 @@ public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilte
 //        }
         ObjectMapper om = new ObjectMapper(); //json 클래스 파싱해줌
         User user = om.readValue(request.getInputStream(), User.class);
+        System.out.println(user);
 
+        //토큰 생성
+        UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(user.getUsername(), user.getPassword());
+
+        //로그인 시도
+        //principalDetailsService의 loadByUsername()함수가 실행됨
+        Authentication authentication = authenticationManager.authenticate(authenticationToken);
+
+        //authentication 객체가 세션영역에 저장됨.
+        //세션 영역에 있는 authentication에 있는 principal이라는 객체를 들고와서
+        //username출력이 되면 => 로그인이 되었다는 뜻
+        PrincipalDetails principalDetails = (PrincipalDetails) authentication.getPrincipal();
+        System.out.println(principalDetails.getUser().getUsername());
+
+        return authentication;
       } catch (IOException e) {
-        throw new RuntimeException(e);
+        e.printStackTrace();
       }
 
       System.out.println("====================");
-
-        return super.attemptAuthentication(request, response);
+      return null;
     }
 }
